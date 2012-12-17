c83ca955675793f7f52391e5cfa91915edf55286
CT Arrington <ct@nowhere.com>
2012-12-17 09:23:55 -0500

    Updated with dropdown

--- TryAngular/Chatter/public/javascripts/chatter.js
+++ TryAngular/Chatter/public/javascripts/chatter.js
@@ -1,5 +1,13 @@
 function ChatterCtrl($scope, $timeout) {
-    $scope.chatter = {name:'Fred', ctr: 0};
+
+    $scope.chatter = {name: null,
+                      ctr: 0,
+                      greetings: ['Yo', 'Hello', 'Hey', 'Hi'],
+                      currentGreeting: 'Yo'};
+
+    $scope.addGreeting = function() {
+        $scope.chatter.greetings.push($scope.newGreeting);
+    };
 
     $scope.$watch('chatter.ctr', function(){
         $timeout(function(){
