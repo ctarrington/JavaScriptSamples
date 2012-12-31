2c03276215d49c56b4ae1479bbc87fbda788f1c2
CT Arrington <ct@nowhere.com>
2012-12-31 08:19:04 -0500

    added dog directive added links to source files in jade templates

--- TryAngular/Chatter/public/javascripts/personDirective.js
+++ TryAngular/Chatter/public/javascripts/personDirective.js
@@ -3,6 +3,7 @@ function PeopleCtrl($scope, $timeout) {
     $scope.people = [{name: 'Fred', age: 34}, {name: 'Ted', age: 35}];
     $scope.newName = null;
     $scope.newAge = null;
+    $scope.barkSound = 'Wooooooof';
 
 
     $scope.addPerson = function() {
@@ -17,6 +18,24 @@ angular.module('appOfThings', []).
             replace: true,
             transclude: true,
             scope: {},
-            template: '<div>Yo from {{$parent.person.name}}</div>'
+            template: '<div>Yo from {{$parent.person.name}} who is {{$parent.person.age}} years old. </div>'
+        }
+    }).
+    directive('dog', function($document) {
+        return {
+            restrict: 'E',
+            replace: true,
+            transclude: true,
+            scope: {name: '@name',
+                    breed: '@breed'
+            },
+            template: '<div ng-transclude>{{name}} the {{breed}} says </div>',
+            link: function($scope, element, attrs) {
+                var currentElement = angular.element(element);
+
+                currentElement.bind('click', function() {
+                        alert('Hi from '+$scope.name);
+                    } );
+            }
         }
     });
