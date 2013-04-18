741dbeb0c1168b4f984c5dab13dd06b791ff5025
CT Arrington <ct@nowhere.com>
2013-04-18 16:43:59 -0400

    Popups

--- TryTimelineJS/js/main.js
+++ TryTimelineJS/js/main.js
@@ -1,1 +1,22 @@
+var timeline_config = {
+    width: "100%",
+    height: "70%",
+    source: 'timelines/chores.jsonp'
+};
+
+function showPDF(daPath)
+{
+    alert('PATH = '+daPath);
+}
+
+function bindToPdf(index, path)
+{
+    $('body').on('click', '#pop'+index, function() { showPDF(path); } );
+}
+
+$(function() {
+   bindToPdf(1, 'pdf1.pdf');
+   bindToPdf(2, 'pdf2.pdf');
+});
+
 
