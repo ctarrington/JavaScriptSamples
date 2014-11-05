3ac2321516d04ba29d6bfafb0131a9130d8504c8
ctarrington <arrington.ct@gmail.com>
2014-11-05 14:33:01 -0500

    messing with json view

--- NodePolymer/public/js/scraps.js
+++ NodePolymer/public/js/scraps.js
@@ -1,2 +1,13 @@
 
 var thingList = document.querySelector('thing-list');
+
+var app = angular.module('app', []);
+
+angular.module('app').controller('ThingsController', function ($scope) {
+    $scope.model = {foo: 'bar'};
+
+    $scope.model.things = [{appearance: {hairColor: 'orange', toothSize: 'huge'}},
+        {appearance: {hairColor: 'blue', toothSize: 'tiny'}},
+        {appearance: {hairColor: 'green', toothSize: 'normal'}},
+        {appearance: {hairColor: 'purple', toothSize: 'large'}}];
+});
