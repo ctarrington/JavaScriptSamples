d19fabedfb887646475115af51c7663c55cfe853
ctarrington <arrington.ct@gmail.com>
2014-12-03 14:28:56 -0500

    make it obvious that there are two distinct charts

--- AngularD3Charts/js/main.js
+++ AngularD3Charts/js/main.js
@@ -28,18 +28,17 @@ angular.module('main').controller('MainController', ['$scope', function($scope) 
 
     var left = [
         {hour: 1,sales: 154},
-        {hour: 2,sales: 177},
-        {hour: 3,sales: 170},
-        {hour: 4,sales: 163},
-        {hour: 6,sales: 147},
-        {hour: 7,sales: 127}
+        {hour: 1.5,sales: 177},
+        {hour: 2,sales: 170},
+        {hour: 2.5,sales: 163},
+        {hour: 3,sales: 147},
+        {hour: 3.5,sales: 127}
     ];
 
     var right = [
-        {hour: 1.5,sales: 169},
-        {hour: 2.5,sales: 179},
-        {hour: 3.5,sales: 173},
-        {hour: 4.5,sales: 151},
+        {hour: 3.5,sales: 169},
+        {hour: 4.0,sales: 179},
+        {hour: 4.5,sales: 173},
         {hour: 5.5,sales: 149},
         {hour: 6.5,sales: 162},
         {hour: 7.5,sales: 130}
