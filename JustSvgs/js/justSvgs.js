b4043e601cee307d5af29607171d7863e14d1a2a
CT Arrington <arrington.ct@gmail.com>
2014-12-15 06:03:41 -0500

    using angular to generate svgs directly

--- JustSvgs/js/justSvgs.js
+++ JustSvgs/js/justSvgs.js
@@ -4,6 +4,7 @@
     angular.module('jsvgs', []);
 
     angular.module('jsvgs').controller('MainController', ['$scope', function($scope) {
+        $scope.data = Lazy.range(1, 249, 249/10).toArray();
 
     }]);
 
