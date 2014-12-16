2ea0a45560fb883990ca7abfde6388d24de112f1
ctarrington <arrington.ct@gmail.com>
2014-12-16 16:10:38 -0500

    grid with ng-repeat

--- JustSvgs/js/justSvgs.js
+++ JustSvgs/js/justSvgs.js
@@ -4,8 +4,10 @@
     angular.module('jsvgs', []);
 
     angular.module('jsvgs').controller('MainController', ['$scope', function($scope) {
-        $scope.data = Lazy.range(1, 249, 249/10).toArray();
-
+        $scope.data = {
+            yvalues: Lazy.range(1, 251, 248 / 10).toArray(),
+            xvalues: Lazy.range(1, 501, 498 / 10).toArray()
+        };
     }]);
 
 })();
