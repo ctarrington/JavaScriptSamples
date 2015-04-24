d5bc8490f5cc1b8bf2dabeb60b5ef6bb380ba13f
ctarrington <arrington.ct@gmail.com>
2015-04-23 20:30:35 -0400

    basic add and delete of cars

--- TryEmber/carstore/js/carstoreApp.js
+++ TryEmber/carstore/js/carstoreApp.js
@@ -1,11 +1,70 @@
 App = Ember.Application.create();
 
+
+App.ApplicationAdapter = DS.LSAdapter.extend({
+	namespace: 'carstore-data'
+});
+
+App.Car = DS.Model.extend({
+	make: DS.attr('string'),
+	carModel: DS.attr('string')
+});
+
 App.Router.map(function() {
-  // put your routes here
+	this.resource('cars', function() {
+		this.route('new');
+		this.route('details', { path: '/:id'});	
+	});
 });
 
 App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue', 'verde'];
-  }
+	beforeModel: function() {
+		this.transitionTo('cars');
+	}
 });
+
+App.CarsRoute = Ember.Route.extend({
+	model: function() {
+		return this.store.find('car');
+	}
+});
+
+App.CarsNewRoute = Ember.Route.extend({
+	model: function() {
+		return Ember.Object.create({});
+	}
+});
+
+App.CarsController = Ember.ObjectController.extend({
+	error: "",
+	actions: {
+		delete: function(car) {
+			car.destroyRecord();
+		}
+	}
+});
+
+App.CarsNewController = Ember.ObjectController.extend({
+	error: "",
+	actions: {
+		newCar: function() {
+			var car = this.get('content');
+
+			if ( (typeof car.get('make') === 'undefined') ||
+			      typeof car.get('carModel') === 'undefined')
+			{
+				this.set('error', 'Please populate all required fields');
+				return;
+			}
+
+			var newCar = this.store.createRecord('car');
+			newCar.set('make', car.get('make'));
+			newCar.set('carModel', car.get('carModel'));
+			newCar.save();
+
+			this.set('error', '');
+			this.transitionToRoute('cars.details', car);
+		}
+	}
+});
+
