f6c391ea4d9c1a78563a2523e84e755bca76005a
CT Arrington <arrington.ct@gmail.com>
2016-03-18 18:50:14 -0400

    lights, shadows, and a little animation

--- TryBabylon/bulb.ts
+++ TryBabylon/bulb.ts
@@ -10,7 +10,7 @@ window.addEventListener('DOMContentLoaded', () => {
         var scene = new BABYLON.Scene(engine);
 
         var camera = new BABYLON.ArcRotateCamera('ArcRotateCamera', 0, 0, 0, BABYLON.Vector3.Zero(), scene);
-        camera.setPosition(new BABYLON.Vector3(0, 7, 14));
+        camera.setPosition(new BABYLON.Vector3(0, 20, 40));
         camera.attachControl(canvas, true, true);
 
         var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(1,1,1), scene);
@@ -42,18 +42,25 @@ window.addEventListener('DOMContentLoaded', () => {
         yellowMaterial.specularColor = new BABYLON.Color3(1, 0.8, 0.8);
         yellowMaterial.alpha = 1.0;
         
+        var glowingMaterial = new BABYLON.StandardMaterial('glowingMaterial', scene);
+        glowingMaterial.diffuseColor = new BABYLON.Color3(0.8, 0.8, 0.2);
+        glowingMaterial.specularColor = new BABYLON.Color3(1, 1, 1);
+        glowingMaterial.emissiveColor = new BABYLON.Color3(0.8, 0.8, 0.2);
+        
         var ground = BABYLON.Mesh.CreateGround('ground', 40, 40, 2, scene);
+        
+        var lightBulbPosition = new BABYLON.Vector3(-5, 1.6, 16);
         
         var largeBlueBox = BABYLON.Mesh.CreateBox('largeBlueBox', 2, scene);
         largeBlueBox.scaling = new BABYLON.Vector3(3,5,1);
         largeBlueBox.position = new BABYLON.Vector3(7, 5, -7);
         largeBlueBox.material = blueMaterial;
         
-        var smallYellowCylinder = BABYLON.Mesh.CreateCylinder('smallYellowCylinder', 5, 3, 4.2, 8, 8, scene);
+        var smallYellowCylinder = BABYLON.Mesh.CreateCylinder('smallYellowCylinder', 5, 3, 4.2, 32, 32, scene);
         smallYellowCylinder.position = new BABYLON.Vector3(2, 2.5, 2);
         smallYellowCylinder.material = yellowMaterial;
         
-        var filamentPosition = new BABYLON.Vector3(0, 1.2, 0);
+        var filamentPosition = lightBulbPosition.add(new BABYLON.Vector3(0, 1.2, 0));
         
         var filamentLight = new BABYLON.PointLight('filamentLight', filamentPosition, scene);
         filamentLight.diffuse = new BABYLON.Color3(1, 1, 1);
@@ -61,7 +68,8 @@ window.addEventListener('DOMContentLoaded', () => {
         filamentLight.intensity = 0.5;
         
         var filamentProxy = BABYLON.Mesh.CreateSphere('filamentProxy', 4, 0.3, scene);
-        filamentProxy.position = filamentPosition;     
+        filamentProxy.position = filamentPosition;   
+        filamentProxy.material = glowingMaterial;  
         
         var base = BABYLON.Mesh.CreateCylinder('base', 1.2, 1.2, 0.9, 20, 8, scene);
         base.position = new BABYLON.Vector3(0, 0, 0);
@@ -76,24 +84,43 @@ window.addEventListener('DOMContentLoaded', () => {
         nib.position = new BABYLON.Vector3(0, -0.5, 0);
         
         var lightBulb = BABYLON.Mesh.CreateBox('lightBulb', 1, scene);
-        lightBulb.position = new BABYLON.Vector3(0, 3.6, 20);
+        lightBulb.position = lightBulbPosition;
         lightBulb.isVisible = false;
         base.parent = lightBulb;
         bulb.parent = lightBulb;
         nib.parent = lightBulb;
-        filamentLight.parent = lightBulb;
-        filamentProxy.parent = lightBulb;
+        
         
         var shadowGenerator = new BABYLON.ShadowGenerator(1024, filamentLight);
         shadowGenerator.usePoissonSampling = false;
-        shadowGenerator.useVarianceShadowMap = false;
-        shadowGenerator.useBlurVarianceShadowMap = true;
+        shadowGenerator.useVarianceShadowMap = true;
+        shadowGenerator.useBlurVarianceShadowMap = false;
         
         var shadowCasters = shadowGenerator.getShadowMap().renderList;
-        shadowCasters.push(smallYellowCylinder, largeBlueBox);
+        shadowCasters.push(smallYellowCylinder, largeBlueBox, nib);
         
-        //largeBlueBox.receiveShadows = true;
+        smallYellowCylinder.receiveShadows = true;
+        largeBlueBox.receiveShadows = true;
         ground.receiveShadows = true;
+        
+        function createAnimation() {
+            var animation = new BABYLON.Animation('animationForBulb', 'position.x', 10, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+            animation.setKeys([
+                {frame:0, value:-5},
+                {frame:100, value:5},
+                {frame: 200, value:-5}
+            ]);
+            
+            return animation;
+        }
+        
+        lightBulb.animations.push(createAnimation());
+        filamentLight.animations.push(createAnimation());
+        filamentProxy.animations.push(createAnimation());
+        
+        scene.beginAnimation(lightBulb, 0, 200, true);
+        scene.beginAnimation(filamentLight, 0, 200, true);
+        scene.beginAnimation(filamentProxy, 0, 200, true);
 
         // return the created scene
         return scene;
@@ -101,6 +128,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // call the createScene function
     var scene = createScene();
+    
+
 
     // run the render loop
     engine.runRenderLoop(() => {
