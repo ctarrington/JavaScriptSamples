0f3b11dc74d5dc030518868bf7e48f9563ba8071
ctarrington <arrington.ct@gmail.com>
2017-03-18 22:38:10 -0400

    refactoring and added LOD based on zoom.

--- d3-refresher/county-pleth/cp.js
+++ d3-refresher/county-pleth/cp.js
@@ -1,24 +1,27 @@
-const svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
+const svg = d3.select("svg");
+const width = +svg.attr("width");
+const height = +svg.attr("height");
 
-const unemployment = d3.map();
+const featureLimit = {
+  COUNTY: 1.3
+};
 
 const path = d3.geoPath();
 
 const colorScale = d3.scaleThreshold()
-    .domain(d3.range(2, 10))
-    .range(d3.schemeBlues[9]);
+  .domain(d3.range(2, 10))
+  .range(d3.schemeBlues[9]);
 
 const mapG = svg.append("g")
-    .attr("class", "map");
+  .attr("class", "map");
+
+const statesG = mapG.append("g")
+  .attr("class", "states");
+
+const countiesG = mapG.append("g")
+  .attr("class", "counties");
 
 addLegend(svg, colorScale);
-
-d3.queue()
-    .defer(d3.json, "https://d3js.org/us-10m.v1.json")
-    .defer(d3.tsv, "unemployment.tsv", function(d) { unemployment.set(d.id, +d.rate); })
-    .await(ready);
 
 function ensure(theObject, key, defaultValue) {
   if (typeof theObject[key] === 'undefined') {
@@ -26,44 +29,79 @@ function ensure(theObject, key, defaultValue) {
   }
 }
 
-function ready(error, us) {
-
-  if (error) throw error;
-
+function rollupData(unemployment) {
+  // aggregate counties to state - simple average not adjusted by population of county.
+  const unemploymentByCounty = {};
   const unemploymentByState = {};
   const countyCountsByState = {};
-  unemployment.keys().forEach((key) => {
-    const stateCode = (''+key).substring(0,2);
-    const value = unemployment['$'+key];
-    ensure(unemploymentByState,'$'+stateCode, 0);
-    ensure(countyCountsByState, '$'+stateCode, 0);
-    unemploymentByState['$'+stateCode] += value;
-    countyCountsByState['$'+stateCode]++;
+
+  unemployment.forEach((county) => {
+    unemploymentByCounty[county.id] = county.rate;
+    const stateCode = county.id.substring(0,2);
+    const value = parseInt(county.rate, 10);
+    ensure(unemploymentByState,stateCode, 0);
+    ensure(countyCountsByState, stateCode, 0);
+    unemploymentByState[stateCode] += value;
+    countyCountsByState[stateCode]++;
   });
+
 
   Object.keys(unemploymentByState).forEach((key) => {
     unemploymentByState[key] = unemploymentByState[key]/countyCountsByState[key];
   });
 
+  return {unemploymentByCounty, unemploymentByState};
+}
+
+function ready(error, us, unemployment) {
+
+  function refresh(scale = 1) {
+    const statePaths = statesG.selectAll('path.state')
+      .data(topojson.feature(us, us.objects.states).features);
+
+    statePaths.enter().append("path")
+        .attr('class', 'state')
+        .attr("d", path)
+        .attr("fill", function(d) {
+          return colorScale(unemploymentByState[d.id]);
+        });
+
+
+    const countyFeatures = (scale > featureLimit.COUNTY)
+      ? topojson.feature(us, us.objects.counties).features
+      : [];
+
+    const countyPaths = countiesG.selectAll("path.county")
+      .data(countyFeatures, (d) => d.id);
+
+    countyPaths.enter().append("path")
+      .attr('class', 'county')
+      .attr("d", path)
+      .attr("fill", function(d) {
+        return colorScale(unemploymentByCounty[d.id]);
+      });
+
+    countyPaths.exit().remove();
+  }
+
+  function zoomed() {
+    mapG.attr("transform", d3.event.transform);
+    refresh(d3.event.transform.k);
+  }
+
+  if (error) throw error;
+
+  const {unemploymentByCounty, unemploymentByState} = rollupData(unemployment);
+
   const zoom = d3.zoom()
       .scaleExtent([1, 40])
       .on("zoom", zoomed);
 
-  const countiesG = mapG.append("g")
-      .attr("class", "counties")
-    .selectAll("path.county")
-    .data(topojson.feature(us, us.objects.counties).features)
-    .enter().append("path")
-      .attr('class', 'county')
-      .attr("d", path)
-      .attr("fill", function(d) {
-        return colorScale(unemployment['$'+d.id]);
-      });
-
   mapG.call(zoom);
-
-  function zoomed() {
-    mapG.attr("transform", d3.event.transform);
-  }
-
+  refresh();
 }
+
+d3.queue()
+    .defer(d3.json, "https://d3js.org/us-10m.v1.json")
+    .defer(d3.tsv, "unemployment.tsv")
+    .await(ready);
