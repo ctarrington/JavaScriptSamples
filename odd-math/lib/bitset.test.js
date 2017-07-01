757baa88cf9f44ed437fa4bea8ee01b543770c99
ctarrington <arrington.ct@gmail.com>
2017-07-01 18:24:49 -0400

    test large bitsets

--- odd-math/lib/bitset.test.js
+++ odd-math/lib/bitset.test.js
@@ -13,11 +13,28 @@ test('small bitset', () => {
 
   bitset.set(2);
   expect(bitset.check(1)).toBe(true);
-  expect(bitset.check(2)).toBe(false);
+  expect(bitset.check(2)).toBe(true);
   expect(bitset.check(3)).toBe(true);
 });
 
 test('out of range bitset', () => {
   const bitset = createBitset(4);
   expect(() => { bitset.check(4); }).toThrow();
+});
+
+test('large bitset', () => {
+  const bitset = createBitset(28124);
+  expect(bitset.check(28121)).toBe(true);
+  expect(bitset.check(28122)).toBe(true);
+  expect(bitset.check(28123)).toBe(true);
+
+  bitset.unset(28122);
+  expect(bitset.check(28121)).toBe(true);
+  expect(bitset.check(28122)).toBe(false);
+  expect(bitset.check(28123)).toBe(true);
+
+  bitset.set(28122);
+  expect(bitset.check(28121)).toBe(true);
+  expect(bitset.check(28122)).toBe(true);
+  expect(bitset.check(28123)).toBe(true);
 });
