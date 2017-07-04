f0f8442a079f22f54336c98d16e3d6d2d31b5e66
ctarrington <arrington.ct@gmail.com>
2017-07-04 15:00:14 -0400

    find hotspot -> abundantNumbersBelow

--- odd-math/test/problems.test.js
+++ odd-math/test/problems.test.js
@@ -10,14 +10,14 @@ const { createPairs } = require('../src/pairs');
 const { createSieveBelow } = require('../src/sieve');
 
 // 23 Non-abundant sums
-test('sum of all positive numbers below 25 which cannot be written as the sum of two abundant numbers.', () => {
-  const abundants = abundantNumbersBelow(25);
-  expect(abundants).toEqual([12, 18, 20, 24]);
+test('sum of all positive numbers below 5000 which cannot be written as the sum of two abundant numbers.', () => {
+  const cap = 5000;
+  const abundants = abundantNumbersBelow(cap); /*?.*/
+  expect(abundants.length).toEqual(1238);
 
-  const pairs = createPairs(abundants);
+  const sieve = createSieveBelow(cap); /*?.*/
+  const pairs = createPairs(abundants); /*?.*/
+  pairs.forEach(p => sieve.remove(p[0] + p[1])); /*?.*/
 
-  const sieve = createSieveBelow(25);
-  expect(sieve.sum()).toEqual(300);
-  pairs.forEach(p => sieve.remove(p[0] + p[1]));
-  expect(sieve.sum()).toEqual(300-24);
+  expect(sieve.sum()).toEqual(2035227);
 });
