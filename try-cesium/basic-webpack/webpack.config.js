92828f18e414e989c8b8f22228ee52f3046eb990
ctarrington <arrington.ct@gmail.com>
2018-10-13 09:04:13 -0400

    add source maps

--- try-cesium/basic-webpack/webpack.config.js
+++ try-cesium/basic-webpack/webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
   entry: {
     app: './src/index.ts'
   },
+  devtool: 'inline-source-map',
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
