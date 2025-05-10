21a980ce56ad6f0dee5301dce81764ac8af7d524
CT Arrington <arrington.ct@gmail.com>
2025-05-10 15:04:44 -0400

    edit and use local storage

--- ag-grid-tree-table/README.md
+++ ag-grid-tree-table/README.md
@@ -6,8 +6,14 @@ npm run dev
 ```
 
 # todo
--[ ] use state and show below
--[ ] add a row
+-[x] use state and show below
+-[x] edit a cell 
+-[x] save to local storage
+-[x] load from local storage
+-[ ] add a top row with no parent
+-[ ] add a row from context with parents preserved
+-[ ] add a group from context with parents preserved
+-[ ] filters
 -[ ] control visibility at group level
 -[ ] control visibility at individual level
 -[ ] reorganize with DnD
--- ag-grid-tree-table/src/App.tsx
+++ ag-grid-tree-table/src/App.tsx
@@ -7,7 +7,7 @@ import type { ColDef } from 'ag-grid-community';
 import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
 
 import { TreeDataModule } from 'ag-grid-enterprise';
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 // Register all Community features
 ModuleRegistry.registerModules([AllCommunityModule, TreeDataModule]);
@@ -20,54 +20,69 @@ type Car = {
     electric: boolean;
 };
 
+const defaultRowData = [
+    {
+        parents: ['foo', 'bar'],
+        name: 'Freddy',
+        make: 'Ford',
+        model: 'Explorer',
+        price: 64950,
+        electric: false,
+    },
+    {
+        parents: ['foo', 'bar'],
+        name: 'Ben',
+        make: 'Ford',
+        model: 'F-150',
+        price: 33850,
+        electric: false,
+    },
+    {
+        parents: ['foo', 'bar'],
+        name: 'Rex',
+        make: 'Toyota',
+        model: 'Corolla',
+        price: 29600,
+        electric: false,
+    },
+    {
+        parents: ['foo', 'boo'],
+        name: 'Dusty',
+        make: 'Toyota',
+        model: 'Camry',
+        price: 39600,
+        electric: true,
+    },
+];
+
+const getRowDataFromLocalStorage = () => {
+    const localRowData = JSON.parse(localStorage.getItem('carRowData') ?? '[]');
+    return localRowData.length > 0 ? localRowData : defaultRowData;
+};
+
 function App() {
     // Row Data: The data to be displayed.
-    const [rowData] = useState<Car[]>([
-        {
-            parents: ['foo', 'bar'],
-            name: 'Freddy',
-            make: 'Ford',
-            model: 'Explorer',
-            price: 64950,
-            electric: false,
-        },
-        {
-            parents: ['foo', 'bar'],
-            name: 'Ben',
-            make: 'Ford',
-            model: 'F-150',
-            price: 33850,
-            electric: false,
-        },
-        {
-            parents: ['foo', 'bar'],
-            name: 'Rex',
-            make: 'Toyota',
-            model: 'Corolla',
-            price: 29600,
-            electric: false,
-        },
-        {
-            parents: ['foo', 'boo'],
-            name: 'Dusty',
-            make: 'Toyota',
-            model: 'Camry',
-            price: 39600,
-            electric: true,
-        },
-    ]);
+    const [rowData, setRowData] = useState<Car[]>(getRowDataFromLocalStorage());
 
     // Column Definitions: Defines the columns to be displayed.
     const colDefs: ColDef[] = [
-        { field: 'make' },
-        { field: 'model' },
-        { field: 'price' },
-        { field: 'electric' },
+        { field: 'make', editable: true },
+        { field: 'model', editable: true },
+        { field: 'price', editable: true },
+        { field: 'electric', editable: true },
     ];
 
     const getDataPath = useCallback((car: Car) => {
         return [...car.parents, car.name];
     }, []);
+
+    const onCellEditingStopped = useCallback(() => {
+        setRowData([...rowData]);
+    }, rowData);
+
+    useEffect(() => {
+        localStorage.setItem('carRowData', JSON.stringify(rowData));
+    }, [rowData]);
 
     return (
         <>
@@ -82,6 +97,7 @@ function App() {
                         autoGroupColumnDef={{
                             cellRendererParams: { suppressCount: true },
                         }}
+                        onCellEditingStopped={onCellEditingStopped}
                     />
                 </div>
                 <div>{JSON.stringify(rowData)}</div>
